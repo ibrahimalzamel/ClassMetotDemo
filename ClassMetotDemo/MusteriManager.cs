7f853609cbd116c2cf3abdda1e91c724b695afd9
ibrahim alzamel <72762688+ibrahimalzamel@users.noreply.github.com>
2021-02-17 19:55:33 +0300

    Update MusteriManager.cs

--- ClassMetotDemo/MusteriManager.cs
+++ ClassMetotDemo/MusteriManager.cs
@@ -7,17 +7,27 @@ namespace ClassMetotDemo
     class MusteriManager
     {
 
+         List<Musteri> musteris = new List<Musteri>();
         public void Add(Musteri musteri)
         {
-            Console.WriteLine(""); 
+            musteris.Add(musteri);
         }
-        public void Listele(Musteri musteri)
+        public void Listele()
         {
-
+            foreach (Musteri musteri in musteris)
+            {
+                Console.WriteLine("Musteri ID : " + musteri.ID);
+                Console.WriteLine("Musteri Adi: " + musteri.Name);
+                Console.WriteLine("Musteri Soyadi : " + musteri.LastName);
+                Console.WriteLine("Musteri Tel : " + musteri.TelNamber);
+                Console.WriteLine("Musteri Address : " + musteri.Address);
+                Console.WriteLine("--------------------------------");
+            }
         }
-        public void Delet(Musteri musteri)
+        public void Delet(Musteri musteri1)
         {
-
+            musteris.Remove(musteri1);
+            Console.WriteLine("=============== Musteri " + musteri1.Name + " Deleted =================");
         }
     }
 }
